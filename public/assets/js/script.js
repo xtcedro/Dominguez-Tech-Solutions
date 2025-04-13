4b002d2cbef3e10b9c8264b02e6868fe443053e5
xtcedro <pedro.dfedro@gmail.com>
2025-04-13 15:26:20 -0500

    update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,11 @@
 // script.js (module)
 import { loadHeader, loadFooter } from './load-components.js';
 import { setupNavigation } from './navigation.js';
+import { initializeChatbot } from './chatbot.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
   loadFooter();
   setupNavigation();
+  initializeChatbot();
 });
