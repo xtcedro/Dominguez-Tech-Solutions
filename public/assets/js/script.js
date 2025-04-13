69db0acdbfbc29d8f9b702236ca91efdfbe1051b
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-13 18:24:51 -0500

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@
 import { loadHeader, loadFooter } from './load-components.js';
 import { setupNavigation } from './navigation.js';
 import { initializeChatbot } from './chatbot.js';
+import { fetchAppointments } from './public-appointments.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
