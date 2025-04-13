875cee3b8903d5ef3991f72de375305e816e9e72
xtcedro <pedro.dfedro@gmail.com>
2025-04-13 14:34:26 -0500

    update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,9 @@
 // script.js (module)
 import { loadHeader, loadFooter } from './load-components.js';
 import { setupNavigation } from './navigation.js';
-import { setupThemeToggle } from './theme-toggle.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
   loadFooter();
   setupNavigation();
-  setupThemeToggle();
 });
