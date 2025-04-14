c8f3421b85e9c3edf2aaa75199330bceaa4d1cce
xtcedro <pedro.dfedro@gmail.com>
2025-04-13 20:43:38 -0500

    update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,14 +2,10 @@
 import { loadHeader, loadFooter } from './load-components.js';
 import { setupNavigation } from './navigation.js';
 import { initializeChatbot } from './chatbot.js';
-import { fetchAppointments } from './public-appointments.js';
-import { deleteAppointment } from './public-appointments.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
   loadFooter();
   setupNavigation();
   initializeChatbot();
-  fetchAppointments();
-  deleteAppointment();
 });
