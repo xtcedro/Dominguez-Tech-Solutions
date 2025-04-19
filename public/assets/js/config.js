f3cb7403640d602b62bbe93d147e61b060257624
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-19 12:15:51 -0500

    Create config.js

---
+++ public/assets/js/config.js
@@ -0,0 +1,7 @@
+// assets/js/config.js
+
+// Static site key for Dominguez Tech Solutions
+export const SITE_KEY = "domtech";
+
+// Construct base URL from current origin (supports http and https)
+export const API_BASE = `${window.location.protocol}//${window.location.host}`;
